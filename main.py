8b8272c668197025c59f1bdcf12e7889727b071c
Daniel Murua <damurua@miuandes.cl>
2026-03-22 20:42:02 -0300

    Definición de funciones de lectura de archivos

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+print ("xdd")
+def leer_recursos(nombre_archivo: str):
+    recursos = []
+    with open(nombre_archivo, "r") as f:
+        for linea in f:
+            
+            partes = linea.strip().split(",")
 
+            recursos.append({
+                "numero": partes[0],
+                "categorias": partes[1:], 
+                "tiempo": 0
+            })
+
+    return recursos
+
+def leer_tareas(nombre_archivo: str):
+    tareas = []
+    with open(nombre_archivo, "r") as f:
+        for linea in f:
+           
+            nom, horas, categoria = linea.strip().split(",")
+            tareas.append({
+                "numero": nom,
+                "duracion": int(horas),
+                "categoria": categoria
+            })
+    return tareas
+print ("xddd")
